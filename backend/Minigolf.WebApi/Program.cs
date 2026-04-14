13bd97e09f5a81608d605b35b2c5abd166445757
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2026-04-14 11:18:25 +0200

    git backend/Minigolf.WebApi/Program.cs

--- backend/Minigolf.WebApi/Program.cs
+++ backend/Minigolf.WebApi/Program.cs
@@ -37,15 +37,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+var allowedOrigins = builder.Configuration["AllowedOrigins"]?.Split(",")
+    ?? ["http://localhost:5173"];
+
 builder.Services.AddCors(options =>
-  {
-      options.AddPolicy("AllowFrontend", policy =>
-      {
-          policy.WithOrigins("http://localhost:5173")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-      });
-  });
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 builder.Services.AddDbContext<AppDbContext>(options =>
       options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -115,8 +118,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("AllowFrontend");
-app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
